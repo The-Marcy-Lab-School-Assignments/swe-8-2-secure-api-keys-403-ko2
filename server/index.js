9e801bc239bcff5ab62d79cc98a7bc6b4297ef27
403-ko2 <zaekotu@gmail.com>
2025-04-05 12:06:24 -0400

    gifs rendered

--- server/index.js
+++ server/index.js
@@ -1,9 +1,10 @@
 //////////////////////////
 // Imports
 //////////////////////////
-
-const path = require('path');
+const dotenv = require('dotenv');
+dotenv.config();
 const express = require('express');
+const path = require('path');
 
 //////////////////////////
 // Constants
@@ -21,8 +22,26 @@ const serveStatic = express.static(pathToDistFolder);
 
 app.use(serveStatic);
 
+const trendingGifs = async (req, res, next) => {
+	const url = `https://api.giphy.com/v1/gifs/trending?limit=3&rating=g&api_key=${process.env.API_KEY}`;
+	console.log('URL:', url);
+	console.log('API KEY:', process.env.API_KEY);
+
+	try {
+		const response = await fetch(url);
+		const data = await response.json();
+
+		res.send(data);
+	} catch (error) {
+		console.error('Error fetching trending gifs:', error);
+		res.status(503).send(error);
+	}
+};
+
+app.get('/api/gifs', trendingGifs);
+
 //////////////////////////
 // Listener
 //////////////////////////
 
-app.listen(port, () => console.log(`listening at http://localhost:${port}`)); 
+app.listen(port, () => console.log(`listening at http://localhost:${port}`));
